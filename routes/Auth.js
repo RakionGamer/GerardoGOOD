2bc0baed61df1aef1027b512bcb9ab55019365fa
RakionG (Ricardo Valera) <101957005+RakionGamer@users.noreply.github.com>
2024-06-17 18:41:25 +0200

    Add files via upload

---
+++ routes/Auth.js
@@ -0,0 +1,102 @@
+require('dotenv').config();
+const jwt = require('jsonwebtoken');
+const { promisify } = require('util');
+const GithubStrategy = require('passport-github').Strategy;
+const passport = require("passport");
+
+// Middleware para proteger rutas
+
+
+
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+passport.deserializeUser((user, done) => {
+    done(null, user);
+});
+
+passport.use(
+    new GithubStrategy(
+        {
+            clientID:process.env.GITHUB_ID,
+            clientSecret:process.env.GITHUB_ID_SECRET,
+            callbackURL: "https://p2-30874540.onrender.com/github/callback",
+            passReqToCallback: true
+        },
+        function (request, accessToken, refreshToken, profile, cb) {
+            cb(null, profile)
+        }
+    )
+);
+
+
+
+exports.githubLogin = passport.authenticate('github');
+
+exports.githubAuth = passport.authenticate('github', { failureRedirect: '/login' }),
+    (req, res) => {
+        res.send({ request: 'Contactos promed..' })
+    }
+
+exports.protectRoute = async (req, res, next) => {
+    const token = req.cookies.jwt;
+    if (token) {
+        try {
+            const tokenAuthorized = await promisify(jwt.verify)(token, process.env.JWTSECRET);
+            if (tokenAuthorized) {
+                req.user = process.env.SECRET;
+                return next();
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    res.redirect("/login");
+};
+
+
+
+
+// Middleware para prevenir el acceso a /login si ya está autenticado
+exports.protectRouteLogOut = async (req, res, next) => {
+    const token = req.cookies.jwt;
+    if (token) {
+        try {
+            const tokenAuthorized = await promisify(jwt.verify)(token, process.env.JWTSECRET);
+            if (tokenAuthorized) {
+                return res.redirect('/contactos');
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    return next();
+};
+
+exports.login = async (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+    if (email == process.env.EMAILUSERSECRET && password == process.env.PASSWORDUSERSECRET) {
+        const id = process.env.SECRET;
+        const token = jwt.sign({ id: id }, process.env.JWTSECRET, { expiresIn: '1h' });
+        res.cookie("jwt", token);
+        res.redirect("/contactos");
+    } else {
+        res.send({
+            request: 'Usted no está autorizado o no existe sus credenciales para ingresar a los contactos...'
+        })
+    }
+
+}
+
+
+
+// Cerrar sesión
+exports.logout = (req, res) => {
+    res.clearCookie("jwt");
+    res.redirect("/login");
+};
+
+
---
+++ routes/index.js
@@ -0,0 +1,40 @@
+require('dotenv').config();
+const express = require('express');
+const router = express.Router();
+const ContactosController = require('./ContactosController');
+const AuthProtect = require('./Auth');
+const controller = new ContactosController();
+
+
+
+router.post('/send', async (req, res) => controller.add(req, res));
+
+router.post('/login',async (req,res) => AuthProtect.login(req,res));
+
+router.get('/logout',async(req,res) => AuthProtect.logout(req,res))
+
+router.get('/github',async(req,res) => AuthProtect.githubLogin(req,res));
+
+router.get('/github/callback',async(req,res) => AuthProtect.githubAuth(req,res));
+
+
+router.get('/contactos',async(req,res) => {
+  const contactos = await controller.model.getContacts();
+  res.render('contactos',{
+    get:contactos
+  })
+})
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+  res.render('index', { 
+    title: 'Express',
+    KEYPUBLIC: process.env.KEY_PUBLIC
+  });
+});
+
+
+
+
+
+module.exports = router;
